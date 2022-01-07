5a4510c66d48efa0671f72ca528e5890dbb1b70f
uronsol <uronsolana@gmail.com>
2022-01-06 20:02:46 -0500

    Fix commented out ipfs file

--- ipfs.ts
+++ ipfs.ts
@@ -27,116 +27,116 @@ interface RootIPFS {
 }
 
 (async () => {
-  // const tokens = fs
-  //   .readdirSync('./metadata/tokens')
-  //   .filter((i) => i.indexOf('render') === -1);
-  // const uploadedTokens: Record<string, string> = {};
-  // const uploadedRenderData: Record<string, string> = {};
-  // const failedTokens: Array<string> = [];
-  // const failedRenderData: Array<string> = [];
+  const tokens = fs
+    .readdirSync('./metadata/tokens')
+    .filter((i) => i.indexOf('render') === -1);
+  const uploadedTokens: Record<string, string> = {};
+  const uploadedRenderData: Record<string, string> = {};
+  const failedTokens: Array<string> = [];
+  const failedRenderData: Array<string> = [];
 
-  // await throttledPromises(
-  //   async (fileId: string) => {
-  //     const path = fileId.split('.');
-  //     const tokenFile = fs
-  //       .readFileSync(`./metadata/tokens/${path[0]}.json`)
-  //       .toString();
-  //     const renderDataFile = fs
-  //       .readFileSync(`./metadata/tokens/${path[0]}-render.json`)
-  //       .toString();
-  //     const token = JSON.parse(tokenFile);
-  //     const tokenId = token.tokenId;
-  //     try {
-  //       const added = await ipfs.add(renderDataFile);
-  //       console.log(`Added renderData ${tokenId}`);
-  //       await ipfs.pin.add(added.cid);
-  //       console.log(`Pinned renderData ${tokenId}`);
-  //       uploadedRenderData[tokenId] = added.path;
-  //       console.log(`Uploaded renderData ${tokenId}`);
-  //     } catch {
-  //       failedRenderData.push(tokenId);
-  //       console.log(`Failed to upload render data ${tokenId}`);
-  //     }
-  //     try {
-  //       const added = await ipfs.add(tokenFile);
-  //       console.log(`Added token ${tokenId}`);
-  //       await ipfs.pin.add(added.cid);
-  //       console.log(`Pinned token ${tokenId}`);
-  //       uploadedTokens[tokenId] = added.path;
-  //       console.log(`Uploaded token ${tokenId}`);
-  //     } catch {
-  //       failedTokens.push(tokenId);
-  //       console.log(`Failed to upload token ${tokenId}`);
-  //     }
-  //   },
-  //   tokens,
-  //   50,
-  //   50
-  // );
+  await throttledPromises(
+    async (fileId: string) => {
+      const path = fileId.split('.');
+      const tokenFile = fs
+        .readFileSync(`./metadata/tokens/${path[0]}.json`)
+        .toString();
+      const renderDataFile = fs
+        .readFileSync(`./metadata/tokens/${path[0]}-render.json`)
+        .toString();
+      const token = JSON.parse(tokenFile);
+      const tokenId = token.tokenId;
+      try {
+        const added = await ipfs.add(renderDataFile);
+        console.log(`Added renderData ${tokenId}`);
+        await ipfs.pin.add(added.cid);
+        console.log(`Pinned renderData ${tokenId}`);
+        uploadedRenderData[tokenId] = added.path;
+        console.log(`Uploaded renderData ${tokenId}`);
+      } catch {
+        failedRenderData.push(tokenId);
+        console.log(`Failed to upload render data ${tokenId}`);
+      }
+      try {
+        const added = await ipfs.add(tokenFile);
+        console.log(`Added token ${tokenId}`);
+        await ipfs.pin.add(added.cid);
+        console.log(`Pinned token ${tokenId}`);
+        uploadedTokens[tokenId] = added.path;
+        console.log(`Uploaded token ${tokenId}`);
+      } catch {
+        failedTokens.push(tokenId);
+        console.log(`Failed to upload token ${tokenId}`);
+      }
+    },
+    tokens,
+    50,
+    50
+  );
 
-  // while (failedTokens.length > 0) {
-  //   const fail = failedTokens.pop();
-  //   const file = fs.readFileSync(`./metadata/tokens/${fail}.json`).toString();
-  //   const fileJson = JSON.parse(file);
-  //   const tokenId = fileJson.tokenId;
-  //   try {
-  //     const added = await ipfs.add(file);
-  //     await ipfs.pin.add(added.cid);
-  //     uploadedTokens[tokenId] = added.path;
-  //     console.log(`Uploaded token ${tokenId}`);
-  //   } catch {
-  //     failedTokens.push(tokenId);
-  //     console.log(`Failed to upload token ${tokenId}`);
-  //   }
-  // }
+  while (failedTokens.length > 0) {
+    const fail = failedTokens.pop();
+    const file = fs.readFileSync(`./metadata/tokens/${fail}.json`).toString();
+    const fileJson = JSON.parse(file);
+    const tokenId = fileJson.tokenId;
+    try {
+      const added = await ipfs.add(file);
+      await ipfs.pin.add(added.cid);
+      uploadedTokens[tokenId] = added.path;
+      console.log(`Uploaded token ${tokenId}`);
+    } catch {
+      failedTokens.push(tokenId);
+      console.log(`Failed to upload token ${tokenId}`);
+    }
+  }
 
-  // while (failedRenderData.length > 0) {
-  //   const fail = failedRenderData.pop();
-  //   const file = fs
-  //     .readFileSync(`./metadata/tokens/${fail}-render.json`)
-  //     .toString();
-  //   const fileJson = JSON.parse(file);
-  //   const tokenId = fileJson.tokenId;
-  //   try {
-  //     const added = await ipfs.add(file);
-  //     await ipfs.pin.add(added.cid);
-  //     uploadedRenderData[tokenId] = added.path;
-  //     console.log(`Uploaded renderData ${tokenId}`);
-  //   } catch {
-  //     failedRenderData.push(tokenId);
-  //     console.log(`Failed to upload render data ${tokenId}`);
-  //   }
-  // }
+  while (failedRenderData.length > 0) {
+    const fail = failedRenderData.pop();
+    const file = fs
+      .readFileSync(`./metadata/tokens/${fail}-render.json`)
+      .toString();
+    const fileJson = JSON.parse(file);
+    const tokenId = fileJson.tokenId;
+    try {
+      const added = await ipfs.add(file);
+      await ipfs.pin.add(added.cid);
+      uploadedRenderData[tokenId] = added.path;
+      console.log(`Uploaded renderData ${tokenId}`);
+    } catch {
+      failedRenderData.push(tokenId);
+      console.log(`Failed to upload render data ${tokenId}`);
+    }
+  }
 
-  // fs.writeFileSync(
-  //   'metadata/ipfs/tokensByIndex.json',
-  //   JSON.stringify(uploadedTokens, null, 2)
-  // );
+  fs.writeFileSync(
+    'metadata/ipfs/tokensByIndex.json',
+    JSON.stringify(uploadedTokens, null, 2)
+  );
 
-  // fs.writeFileSync(
-  //   'metadata/ipfs/renderDataByIndex.json',
-  //   JSON.stringify(uploadedRenderData, null, 2)
-  // );
+  fs.writeFileSync(
+    'metadata/ipfs/renderDataByIndex.json',
+    JSON.stringify(uploadedRenderData, null, 2)
+  );
 
-  // const tokenHashArray: string[] = [];
-  // const renderDataArray: string[] = [];
+  const tokenHashArray: string[] = [];
+  const renderDataArray: string[] = [];
 
-  // for (let i = 0; i < Object.keys(uploadedTokens).length; i++) {
-  //   const index = `${i + 1}`;
-  //   const token = uploadedTokens[index];
-  //   const renderData = uploadedRenderData[index];
-  //   tokenHashArray.push(token);
-  //   renderDataArray.push(renderData);
-  // }
+  for (let i = 0; i < Object.keys(uploadedTokens).length; i++) {
+    const index = `${i + 1}`;
+    const token = uploadedTokens[index];
+    const renderData = uploadedRenderData[index];
+    tokenHashArray.push(token);
+    renderDataArray.push(renderData);
+  }
 
-  // fs.writeFileSync(
-  //   'metadata/ipfs/tokensAsArray.json',
-  //   JSON.stringify(tokenHashArray)
-  // );
-  // fs.writeFileSync(
-  //   'metadata/ipfs/renderDataAsArray.json',
-  //   JSON.stringify(renderDataArray)
-  // );
+  fs.writeFileSync(
+    'metadata/ipfs/tokensAsArray.json',
+    JSON.stringify(tokenHashArray)
+  );
+  fs.writeFileSync(
+    'metadata/ipfs/renderDataAsArray.json',
+    JSON.stringify(renderDataArray)
+  );
 
   const tokensByIndexFile = fs.readFileSync('metadata/ipfs/tokensByIndex.json');
   const renderDataByIndexFile = fs.readFileSync(
